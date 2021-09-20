9bb22aa911336a101bfecefe851d29ba6034500b
Dhugald Spry (1000040435) <SPRYDL1@student.op.ac.nz>
2021-09-20 14:22:57 +1200

    Start of the app for the controlling of the cameras and outputing the data '

---
+++ PythonUIApp/Testapp.py
@@ -0,0 +1,51 @@
+import sys
+import PySimpleGUI as sg
+import cv2
+import time
+# Send Action to Control Rover
+def rover(action):
+    if action == "FORWARD":
+        print('F')
+    if action == "LEFT":
+        print('L')
+    if action == "RIGHT":
+        print('R')
+    if action == "STOP":
+        print('S')
+
+# All the stuff inside your window.
+font = "Ariel 32"
+col =  [[sg.Text(" ",size=(20,1) , key="feedback")],
+[sg.Button("UP", size=(5,1), font=font)],
+[sg.Button("LEFT", size=(5,1), font=font),sg.Button("RIGHT", size=(5,1), font=font)],
+[sg.Button("DOWN", size=(5,1), font=font)],
+[sg.Button("QUIT")]
+]
+image=[[sg.Image(filename='', key='cam')]]
+
+layout = [
+    [sg.Column(image, element_justification='c', justification='c')],
+    [sg.Column(col, element_justification='c', justification='c')]
+]
+# Create the Window
+cap = cv2.VideoCapture(0)
+window = sg.Window('PySimpleGUI Rover Control', layout, size=(1920, 1080), no_titlebar=False, alpha_channel=1, grab_anywhere=False, resizable=True)
+
+
+# Event Loop to process "events" and pass them to the rover function
+
+while True:
+    start_time = time.time()
+    event, values = window.read(timeout=20)
+    print(event,values)
+    if event in (None, 'QUIT'): # if user closes window or clicks cancel
+        break
+    if event != '__TIMEOUT__':
+        window['feedback'].Update(event) # show the button in the feedback text
+    ret, frame = cap.read()                               # Read image from capture device (camera)
+    imgbytes=cv2.imencode('.png', frame)[1].tobytes()     # Convert the image to PNG Bytes
+    window['cam'].update(data=imgbytes)   # Change the Image Element to show the new image
+    rover(event)
+    
+cap.release()
+window.close() # exit cleanly
